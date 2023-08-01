a0653d453049227923cb6c33db21fe9d5d1a1d7b
Gustavo Vega <gux.vega@gmail.com>
2023-08-01 21:34:07 +0000

    more code for auth feature

--- src/app/components/auth.tsx
+++ src/app/components/auth.tsx
@@ -1,4 +1,5 @@
 import SignIn from "./signin";
+import SignUp from "./signup";
 
 export default function Auth() {
   return (
@@ -11,10 +12,10 @@ export default function Auth() {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <SignIn/>
+            <SignUp/>
             <div className="flex">
               <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 m-1 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign In</button>
               <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 m-1 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign Up</button>
-
             </div>
           </form>
         </div>
