7d2b0306b272cf71b0abb4619fe96df35cc28f57
Gustavo Vega <gux.vega@gmail.com>
2023-08-02 16:55:57 -0700

    added table and modals

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -331,7 +331,7 @@ export default function Home() {
             <button
               type="submit"
               className={`flex w-full justify-center rounded-md bg-indigo-600 mt-10 p-4 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600`}
-              onClick={handleSignIn}
+              onClick={(e) => handleSignIn(e)}
 
             >
               Submit
